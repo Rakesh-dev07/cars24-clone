dcbab89f416a14526e5557bb82a5150feeaa8722
Rakesh-dev07 <ry57520s@gmail.com>
2026-07-25 11:41:49 +0530

    feat: calculate recommended price for car details endpoint

--- Cars24API/Services/CarService.cs
+++ Cars24API/Services/CarService.cs
@@ -6,18 +6,37 @@ namespace Cars24API.Services
     public class CarService
     {
         private readonly IMongoCollection<Car> _cars;
-        public CarService(IConfiguration config)
+        private readonly DynamicPricingService _dynamicPricingService;
+        public CarService(
+            IConfiguration config,
+            DynamicPricingService dynamicPricingService)
         {
             var client = new MongoClient(config.GetConnectionString("Cars24DB"));
 
             var database = client.GetDatabase(config["MongoDB:DatabaseName"]);
             _cars = database.GetCollection<Car>("Cars");
+
+            _dynamicPricingService = dynamicPricingService;
         }
         public async Task<List<Car>> GetAllAsync() =>
             await _cars.Find(_ => true).ToListAsync();
         public async Task<Car?> GetByIdAsync(string id)
         {
-            return await _cars.Find(u => u.Id == id).FirstOrDefaultAsync();
+            var car = await _cars
+                .Find(u => u.Id == id)
+                .FirstOrDefaultAsync();
+
+            if (car != null)
+            {
+                car.RecommendedPrice =
+                    _dynamicPricingService.CalculateRecommendedPrice(
+                        car.Price,
+                        car.City,
+                        car.Title
+                    );
+            }
+
+            return car;
         }
         public async Task CreateAsync(Car car) =>
             await _cars.InsertOneAsync(car);
